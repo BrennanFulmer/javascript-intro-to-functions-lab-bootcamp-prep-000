6bf1c53f7358ecc18d27ea0750320e999f5ee847
Brennan Fulmer <brennan.flmr@gmail.com>
2018-03-19 23:32:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"
   } else if (string !== string.toLowerCase()) {
     return "YES INDEED!"
-  } else if () {
+  } else if (string ) {
     
   }
 }
