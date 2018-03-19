5e6b042e90146af9a0df16571aad727243c5e053
Brennan Fulmer <brennan.flmr@gmail.com>
2018-03-19 23:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
   if (string == string.toLowerCase()) {
     return "I can't hear you!"
   } else if (string == string.toUpperCase()) {
@@ -23,5 +22,4 @@ function sayHiToGrandma(string) {
   } else if (string == "I love you, Grandma.") {
     return "I love you, too."
   }
-  
 }
